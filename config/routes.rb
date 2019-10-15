0d64d486c5f382067857c14fe08dccfee6fbc634
Felix <felixaviermartinez@gmail.com>
2019-10-15 17:03:33 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :posts, except: [:destroy]
+
+  patch 'posts/:id', to: 'posts#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
